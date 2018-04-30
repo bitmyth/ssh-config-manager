e37e24cd635484277b96ee44367205d3358f7f23
bitmyth <fishis@163.com>
2018-04-30 16:42:26 +0800

    haha

--- zoo.sh
+++ zoo.sh
@@ -1,46 +1,108 @@
-function update(){
-	# package manager
-	pm="apt";
-	# update
-	eval "$pm update"
-}
 
-#preinstalled applications array
-apps=()
-
+lines=()
+index=0
+known_hosts_arr=()
 function parseEnv(){
-	if [ -f .env ]; then
-	for line in `cat .env`
-	do
-	#echo $line
-	apps[${#apps[*]} + 1]=$line
-	done
+	if [ -f known_hosts ]; then
+	#cat known_hosts |awk  '{print $1}' > h
+	#rm h -f
+
+	while read line;do
+		#$line=` echo $line |awk  '{print $1}'`
+		lines[${#lines[*]}]="$line"
+		known_hosts_arr[${#known_hosts_arr[*]}]=`echo $line|awk '{print $1}'`
+		#echo $line
+	done < known_hosts
+
+	else
+		echo no file know_hosts found
+		exit
 	fi
 }
 
 parseEnv
 
+function saveCurrentTtySettings(){
+	old_stty_settings=`stty -g`
+}
+function recoverCurrentTtySettings(){
+	stty "$old_stty_settings"
+}
+function moveCursorToOrigin(){
+	echo -en "\e[0;0H"
+}
 
-for v in ${apps[@]};do
-apt-get install $v  -y 2>&1 1>/dev/null
-echo -e "\e[32m installed $v \e[0m"
+function clearScreen(){
+	echo -e "\e[2J"
+}
+
+function repaint(){
+	echo -en "\e[2J\e[0;0H"
+}
+
+
+function up(){
+	echo -n ${lines[$index]}
+	echo -en "\r" #carrige return
+	echo -en "\e[25" #
+	let index=index-1
+	[ $index -lt 0 ]&& let index=0
+	echo -en "\e[1A" #up
+	oneline
+}
+
+function down(){
+	echo -n ${lines[$index]}
+	echo -en "\r" #carrige return
+	echo -en "\e[25" #
+	let index=index+1
+	echo -en "\e[1B" #down
+	oneline
+}
+function oneline(){
+	echo -en "\e[31m" #red font color
+	echo -n ${lines[$index]}
+	echo -en "\r"
+	echo -en "\e[m" #reset font style
+}
+
+
+function printLines(){
+	for (( i=0;i<=${#lines[@]};i++ ));do
+		echo  ${lines[$i]}         
+	done
+}
+
+function page(){
+	repaint
+	printLines
+}
+
+function sshConnect(){
+	recoverCurrentTtySettings&&repaint
+	#eval ssh ${know_hosts[$index]}
+	echo connecting  ${known_hosts_arr[$index]} 
+	ssh ${known_hosts_arr[$index]} 
+}
+
+####################################################################################################
+
+saveCurrentTtySettings
+stty -echo
+page
+moveCursorToOrigin
+
+while true
+	do
+	dd if=/dev/tty bs=1 count=1 of=keypress  &>/dev/null
+
+	keypress=`cat keypress`
+	case "$keypress" in
+	 [j] ) down;;
+	 [k] ) up;;
+	 [q] ) recoverCurrentTtySettings&&repaint&& exit;;
+	 * )  break ;;
+	esac
 done
 
-echo ${apps[0]}
-
-export EDITOR=vim
-export HISTTIMEFORMAT="%m/%d/%y %T "
-
-
-#cat <<EOF
-#'aaa'
-#EOF
-
-for (( i=2;i<=$#;i++ )); do
-	eval echo  "\${$i}"         
-done
-
-if [[  $1 =~ ^-[^rf] ]]; then
-    echo "usage: rm [-rf] files"
-    exit 1
-fi
+sshConnect
